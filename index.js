cbebc5f1f7f1f99e3304cb88b3870caf6b8907ae
shrikant kallshetty <kalshettyshrikant@gmail.com>
2023-04-05 15:00:10 +0530

    master

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+
+const app = express();
+
+const url = 'mongodb+srv://kalshettyshrikant:shrikant@cluster0.atojjzi.mongodb.net/?retryWrites=true&w=majority';
+const dbName = 'binary-tree-db';
+const collectionName = 'binary-tree';
+
+async function breadthFirstSearch(startNodeValue) {
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection(collectionName);
+
+  const queue = [startNodeValue];
+  const visited = new Set();
+
+  while (queue.length > 0) {
+    const curr = queue.shift();
+
+    if (visited.has(curr)) {
+      continue;
+    }
+
+    const node = await collection.findOne({ value: curr });
+
+    console.log(node.value);
+
+    visited.add(curr);
+
+    if (node.left) {
+      queue.push(node.left.value);
+    }
+
+    if (node.right) {
+      queue.push(node.right.value);
+    }
+  }
+
+  client.close();
+}
+
+app.get('/breadth-first-search/:startNode', (req, res) => {
+  const startNode = req.params.startNode;
+
+  breadthFirstSearch(startNode)
+    .then(() => {
+      res.send('Breadth-first search completed.');
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send('internal server error')})});
+
+      app.listen(3000, () => {
+        console.log('Server started on port 3000');
+        });
---
+++ insertData.js
@@ -0,0 +1,118 @@
+// const { MongoClient } = require('mongodb');
+// const client =  MongoClient.connect('mongodb+srv://kalshettyshrikant:shrikant@cluster0.atojjzi.mongodb.net/?retryWrites=true&w=majority');
+// const db = client.db("binary-tree-db");
+// const collection = db.collection("binary-tree");
+
+// const nodes = [
+//   {
+//     value: 'A',
+//     left: {
+//       value: 'B',
+//       left: {
+//         value: 'D',
+//       },
+//       right: {
+//         value: 'E',
+//       },
+//     },
+//     right: {
+//       value: 'C',
+//       left: {
+//         value: 'F',
+//       },
+//     },
+//   },
+//   {
+//     value: 'B',
+//     left: {
+//       value: 'D',
+//     },
+//     right: {
+//       value: 'E',
+//     },
+//   },
+//   {
+//     value: 'C',
+//     left: {
+//       value: 'F',
+//     },
+//   },
+//   {
+//     value: 'D',
+//   },
+//   {
+//     value: 'E',
+//   },
+//   {
+//     value: 'F',
+//   },
+// ];
+
+// collection.insertMany(nodes);
+
+// client.close();
+
+const { MongoClient } = require('mongodb');
+
+async function connectToDatabase() {
+  const uri = 'mongodb+srv://kalshettyshrikant:shrikant@cluster0.atojjzi.mongodb.net/?retryWrites=true&w=majority';
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect();
+  const database = client.db('binary-tree-db');
+  return database;
+}
+
+async function insertData() {
+  const database = await connectToDatabase();
+  const collection = database.collection('binary-tree');
+
+  // Insert data into collection
+  const nodes = [
+    {
+      value: 'A',
+      left: {
+        value: 'B',
+        left: {
+          value: 'D',
+        },
+        right: {
+          value: 'E',
+        },
+      },
+      right: {
+        value: 'C',
+        left: {
+          value: 'F',
+        },
+      },
+    },
+    {
+      value: 'B',
+      left: {
+        value: 'D',
+      },
+      right: {
+        value: 'E',
+      },
+    },
+    {
+      value: 'C',
+      left: {
+        value: 'F',
+      },
+    },
+    {
+      value: 'D',
+    },
+    {
+      value: 'E',
+    },
+    {
+      value: 'F',
+    },
+  ];
+  
+  await collection.insertMany(nodes);
+}
+
+insertData();
